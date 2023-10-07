51d56bd754dd8a6f16b1ce2b85b2913ff3bb1dcb
TechnicZ Truestory <43160410+khairultruestory@users.noreply.github.com>
2023-10-08 03:11:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-// Connect to the WebSocket server
-const socket = new WebSocket('wss://stream.coingecko.com/v2/coins');
+// Connect to the CoinGecko WebSocket API
+const socket = new WebSocket('wss://stream.coingecko.com/')
 
-// Define the currencies and exchanges you want to track
-const currencies = ['bitcoin', 'ethereum', 'solana'];
-const exchanges = ['dydx', 'uniswap', '1inch'];
+// Define the message to subscribe to the required cryptocurrencies
+const subscriptionMessage = {
+    type: 'subscribe',
+    channels: [
+        {
+            name: 'ticker',
+            product_ids: ['bitcoin', 'ethereum', 'solana', 'dydx', 'uniswap', '1inch']
+        }
+    ]
+}
 
-// Listen for messages from the WebSocket server
+socket.onopen = () => {
+    socket.send(JSON.stringify(subscriptionMessage))
+}
+
 socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    
-    // Update the table with real-time prices
-    currencies.forEach((currency) => {
-        exchanges.forEach((exchange) => {
-            const rowId = `${currency}-${exchange}`;
-            const row = document.getElementById(rowId);
-            
-            if (row) {
-                const price = data[`${currency}-${exchange}`].usd;
-                row.cells[2].textContent = price.toFixed(2);
-            }
-        });
-    });
-};
-
-// Handle WebSocket connection errors
-socket.onerror = (error) => {
-    console.error('WebSocket error:', error);
-};
-
-// Handle WebSocket connection closure
-socket.onclose = (event) => {
-    if (event.wasClean) {
-        console.log('WebSocket connection closed cleanly, code=' + event.code + ', reason=' + event.reason);
-    } else {
-        console.error('WebSocket connection abruptly closed');
+    const data = JSON.parse(event.data)
+    if (data.type === 'ticker') {
+        const { product_id, price } = data.data
+        const [crypto, base] = product_id.split('-')
+        const cellId = `${crypto.toLowerCase()}-${base.toLowerCase()}-price`
+        document.getElementById(cellId).textContent = price
     }
-};
+}
