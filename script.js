32bea3476290367ee1aace58b3337d4eff019f21
TechnicZ Truestory <43160410+khairultruestory@users.noreply.github.com>
2023-10-18 00:19:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const TronWeb = require('tronweb'); // If using npm
+// Include TronWeb
+const TronWeb = require('tronweb');
 const HttpProvider = TronWeb.providers.HttpProvider;
 
 const tronWeb = new TronWeb({
   fullHost: 'https://api.trongrid.io', // Replace with your chosen Tron node
 });
 
-// Now you can use tronWeb to interact with the Tron blockchain.
-
 // Define the API endpoint for CoinGecko
 const apiUrl = 'https://api.coingecko.com/api/v3/simple/price';
 
 // Define the parameters for the request
 const params = {
-  ids: 'bitcoin,ethereum,chainlink,cardano,vechain', // Added Chainlink, Cardano, and VeChain
+  ids: 'bitcoin,ethereum,chainlink,cardano,vechain', // Cryptocurrency IDs
   vs_currencies: 'usdc', // Specify USDC as the desired currency
 };
 
@@ -27,6 +26,8 @@ fetch(url)
     // Process the data here, and update your webpage
     console.log(data);
   })
-  .catch error => {
+  .catch(error => {
     console.error('Error fetching data:', error);
   });
+
+// Now, you can use tronWeb for blockchain interactions in your project.
